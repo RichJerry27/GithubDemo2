ca33409db512ab240d7ec1cf18960bf84f4e3d56
Richard Jerry <10014429@sbschools.org>
2018-11-27 12:53:14 -0500

    firstmethod
    basic setup

---
+++ GithubPractice.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class GithubPractice{
+
+	public static void main (String[]args){
+
+	ArrayList <Integer> list1 = new ArrayList <> ();
+	ArrayList <Integer> list2 = new ArrayList <> ();
+
+	for (int i = 0; i < 10; i++){
+
+		list1.add(i);
+		list2.add(i+10);
+
+	}
+
+	System.out.println(Combine(list1,list2));
+
+	}
+
+	public static ArrayList <Integer> Combine ( ArrayList <Integer> list1 , ArrayList <Integer> list2){
+
+	ArrayList <Integer> list3 = new ArrayList <> ();
+
+
+	return list1;
+
+	}
+	}
